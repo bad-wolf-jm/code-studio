ebf9b31f61dcee263e747dcae5fa7097e2ad6d0b
Jean-Martin Albert <jm.albert@gmail.com>
2024-05-09 21:37:02 -0400

    Commented out problematic code

--- CodeStudio/ToolKit.UI.Controls/TreeView/Converters/TreeLevelToMarginConverter.cs
+++ CodeStudio/ToolKit.UI.Controls/TreeView/Converters/TreeLevelToMarginConverter.cs
@@ -4,23 +4,22 @@ using Avalonia.Data.Converters;
 using System;
 using System.Globalization;
 
-namespace Metrino.Development.Studio.Library.Converters
+namespace Metrino.Development.Studio.Library.Converters;
+
+public class TreeLevelToMarginConverter : IValueConverter
 {
-    public class TreeLevelToMarginConverter : IValueConverter
+    public double Indent { get; set; } = 15;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        public double Indent { get; set; } = 15;
+        var level = (int)value;
+        var margin = new Thickness(level * Indent, 0, 0, 0);
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var level = (int)value;
-            var margin = new Thickness(level * Indent, 0, 0, 0);
+        return margin;
+    }
 
-            return margin;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return null;
-        }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
     }
 }
