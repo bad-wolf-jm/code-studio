bf3b3d098ac346bad7a4bb587c2e65d120d1e3bd
Jean-Martin Albert <jm.albert@gmail.com>
2024-05-13 20:48:30 -0400

    Removed unnecessaty using statements

--- CodeStudio/CodeStudio.Core/FuzzyFilter.cs
+++ CodeStudio/CodeStudio.Core/FuzzyFilter.cs
@@ -1,13 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using LibGit2Sharp;
 using Metrino.Development.Core;
 using Metrino.Development.UI.Core;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeStudio.Core;
 
